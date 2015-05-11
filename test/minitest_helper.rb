435aa6c19d6144bb8e9367d8714dfb2ff34d38c5
toshi <me@toshimaru.net>
2015-05-12 00:57:55 +0900

    Add test task and minitest-reporter

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,4 +1,7 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ruby/short_url'
 
+require "minitest/reporters"
+Minitest::Reporters.use!
+
 require 'minitest/autorun'
--- test/ruby/test_short_url.rb
+++ test/ruby/test_short_url.rb
@@ -1,11 +1,11 @@
 require 'minitest_helper'
 
-class TestRuby::ShortUrl < Minitest::Test
+class TestShortUrl < Minitest::Test
   def test_that_it_has_a_version_number
     refute_nil ::Ruby::ShortUrl::VERSION
   end
 
   def test_it_does_something_useful
-    assert false
+    assert true
   end
 end
