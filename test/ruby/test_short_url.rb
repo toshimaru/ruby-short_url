8a3752efdf7e45af486640f0b02cee567da66d9c
toshi <me@toshimaru.net>
2015-05-15 01:28:40 +0900

    Add tests

--- test/ruby/test_short_url.rb
+++ test/ruby/test_short_url.rb
@@ -1,11 +1,29 @@
 require 'minitest_helper'
 
-class TestShortUrl < Minitest::Test
-  def test_that_it_has_a_version_number
+describe Ruby::ShortUrl do
+  let(:encoder) { Ruby::ShortUrl::Encoder.new }
+
+  it "has a version number" do
     refute_nil ::Ruby::ShortUrl::VERSION
   end
 
-  def test_it_does_something_useful
-    assert true
+  it 'responds to methods#encode_url' do
+    assert encoder.respond_to? :encode_url
+  end
+
+  it 'responds to methods#decode_url' do
+    assert encoder.respond_to? :decode_url
+  end
+
+  describe "original value and decoded value are same" do
+    let(:values) { [1, 12, 123, 1234, 1234567890] }
+
+    it "has same value" do
+      values.each do |i|
+        encoded = encoder.encode_url(i)
+        decoded = encoder.decode_url(encoded)
+        assert_equal decoded, i
+      end
+    end
   end
 end
