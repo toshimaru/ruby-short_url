d4b2038191cb8985df54dd410575668640c948ed
toshi <me@toshimaru.net>
2015-05-15 01:44:52 +0900

    Coverage :star2:

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -1,7 +1,11 @@
+require 'simplecov'
+SimpleCov.start "test_frameworks" do
+  add_group "Libraries", "lib"
+end
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ruby/short_url'
 
 require "minitest/reporters"
 Minitest::Reporters.use!
-
 require 'minitest/autorun'
