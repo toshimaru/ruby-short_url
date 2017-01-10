9e26f259fc136a8b5728308b266cbe8c5f0f75fc
Toshimaru <me@toshimaru.net>
2017-01-11 03:55:21 +0900

    Ruby-Ish code

--- test/minitest_helper.rb
+++ test/minitest_helper.rb
@@ -4,6 +4,6 @@ SimpleCov.start
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'ruby-short_url'
 
-require "minitest/reporters"
+require 'minitest/reporters'
 Minitest::Reporters.use!
 require 'minitest/autorun'
