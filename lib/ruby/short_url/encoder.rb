b4cc2a8dd29c270fcc4d32cc683c06d48fb9b9a6
Toshimaru <me@toshimaru.net>
2017-01-11 04:02:37 +0900

    Revert "Ruby-Ish code"
    This reverts commit 9e26f259fc136a8b5728308b266cbe8c5f0f75fc.

--- lib/ruby/short_url/encoder.rb
+++ lib/ruby/short_url/encoder.rb
@@ -8,7 +8,7 @@ module Ruby
 
       def initialize(alphabet: DEFAULT_ALPHABET, block_size: DEFAULT_BLOCK_SIZE)
         raise ArgumentError.new 'alphabet must contain at least 2 characters.' if alphabet.length < 2
-        raise ArgumentError.new 'block_size must be greater than 0.' if block_size.zero? || block_size.negative?
+        raise ArgumentError.new 'block_size must be greater than 0.' unless block_size > 0
 
         @alphabet = alphabet
         @block_size = block_size
@@ -45,7 +45,7 @@ module Ruby
       def debase(x)
         n = @alphabet.length
         x.split('').reverse.each_with_index.inject(0) do |result, (c, i)|
-          result + @alphabet.index(c) * (n**i)
+          result += @alphabet.index(c) * (n ** i)
         end
       end
 
@@ -62,7 +62,7 @@ module Ruby
       end
 
       def _calc_result(result, n, x, y)
-        return result |= (1 << x) if (n & (1 << y)).positive?
+        return result |= (1 << x) if n & (1 << y) != 0
         result
       end
 
